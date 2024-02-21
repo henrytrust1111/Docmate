ee217f7306dada058b1758d436ae7b42ed1defba
henrytrust1111 <henrytrust1111@gmail.com>
2024-02-21 20:17:27 +0100

    progress on dashboard

--- src/Components/Dashboard/AdminRightNav.jsx
+++ src/Components/Dashboard/AdminRightNav.jsx
@@ -2,34 +2,52 @@ import React from "react";
 import "./AdminRightNav.css";
 
 const AdminRightNav = () => {
-  return <div className="admin-dashboard-right">
-    <div className="admin-dashboard-right-wrapper">
-      <div className="dashboard-overview">
-        <h2>Dashboard Overview</h2>
-      </div>
-      <div className="dashboard-main-content-container">
-        <div className="select-dashboard-view-container">
-          <div className="select-dashboard-view-wrapper">
-            <p>11</p>
-            <p>All Appointment</p>
-          </div>
-          <div className="select-dashboard-view-wrapper">
-          <p>11</p>
-            <p>Reschedules</p>
-          </div>
-          <div className="select-dashboard-view-wrapper">
-          <p>11</p>
-            <p>Attended</p>
-          </div>
-          <div className="select-dashboard-view-wrapper">
-          <p>11</p>
-            <p>Unattended</p>
-          </div>
+  return (
+    <div className="admin-dashboard-right">
+      <div className="admin-dashboard-right-wrapper">
+        <div className="dashboard-overview">
+          <h2>Dashboard Overview</h2>
         </div>
-        <div className="selected-view-container"></div>
+        <div className="dashboard-main-content-container">
+          <div className="select-dashboard-view-container">
+            <div className="select-dashboard-view-wrapper" id="active-state">
+              <div className="select-dashboard-view-holder active-state-container">
+                <div>
+                  <p>11</p>
+                  <p>All Appointment</p>
+                </div>
+              </div>
+            </div>
+            <div className="select-dashboard-view-wrapper">
+              <div className="select-dashboard-view-holder">
+                <div>
+                  <p>11</p>
+                  <p>Reschedules</p>
+                </div>
+              </div>
+            </div>
+            <div className="select-dashboard-view-wrapper">
+              <div className="select-dashboard-view-holder">
+                <div>
+                  <p>11</p>
+                  <p>Attendance</p>
+                </div>
+              </div>
+            </div>
+            <div className="select-dashboard-view-wrapper">
+              <div className="select-dashboard-view-holder">
+                <div>
+                  <p>11</p>
+                  <p>Unattended</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="selected-view-container"></div>
+        </div>
       </div>
     </div>
-  </div>;
+  );
 };
 
 export default AdminRightNav;

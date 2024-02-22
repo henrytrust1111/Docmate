d61634250d0ddc3e07db4274e34189a7413cabe8
henrytrust1111 <henrytrust1111@gmail.com>
2024-02-22 16:06:36 +0100

    dashboard in progress

--- src/Components/Dashboard/AdminRightNav.jsx
+++ src/Components/Dashboard/AdminRightNav.jsx
@@ -44,8 +44,207 @@ const AdminRightNav = () => {
             </div>
           </div>
           <div className="selected-view-container">
-            <div className="dashboard-attribute-container"></div>
-            <div className="dashboard-list-of-items-container"></div>
+            <div className="dashboard-attribute-container">
+              <div className="dashboard-attribute-wrapper">
+                <div className="s-n">S/N</div>
+                <div className="attribute-fixed-width patient-attribute">
+                  PATIENT
+                </div>
+                <div className="attribute-fixed-width email-attribute">
+                  EMAIL
+                </div>
+                <div className="attribute-fixed-width request-attribute">
+                  REQUEST
+                </div>
+                <div className="attribute-fixed-width date-attribute">DATE</div>
+              </div>
+              <div className="dashboard-list-wrapper">
+                <div className="list-container">
+                  <div className="s-n">1</div>
+                  <div className="attribute-fixed-width patient-profile-record">
+                    <div className="profile-img-view"></div>
+                    <div className="name-view-profile-container">
+                      <p>Micheal Adekunle</p>
+                      <p className="colored-view-profile">View profile</p>
+                    </div>
+                  </div>
+                  <div className="attribute-fixed-width email-record">
+                    Vivian@gmail.com
+                  </div>
+                  <div className="attribute-fixed-width request-record">
+                    Appointment
+                  </div>
+                  <div className="attribute-fixed-width date-record">
+                    Today, 10 May 2023
+                  </div>
+                  <div className="delete-view-btn">
+                    <div className="view-button">View</div>
+                    <div className="delete-button">Delete</div>
+                  </div>
+                </div>
+                <div className="list-container">
+                  <div className="s-n">2</div>
+                  <div className="attribute-fixed-width patient-profile-record">
+                    <div className="profile-img-view"></div>
+                    <div className="name-view-profile-container">
+                      <p>Micheal Adekunle</p>
+                      <p className="colored-view-profile">View profile</p>
+                    </div>
+                  </div>
+                  <div className="attribute-fixed-width email-record">
+                    Vivian@gmail.com
+                  </div>
+                  <div className="attribute-fixed-width request-record">
+                    Appointment
+                  </div>
+                  <div className="attribute-fixed-width date-record">
+                    Today, 10 May 2023
+                  </div>
+                  <div className="delete-view-btn">
+                    <div className="view-button">View</div>
+                    <div className="delete-button">Delete</div>
+                  </div>
+                </div>
+                <div className="list-container">
+                  <div className="s-n">3</div>
+                  <div className="attribute-fixed-width patient-profile-record">
+                    <div className="profile-img-view"></div>
+                    <div className="name-view-profile-container">
+                      <p>Micheal Adekunle</p>
+                      <p className="colored-view-profile">View profile</p>
+                    </div>
+                  </div>
+                  <div className="attribute-fixed-width email-record">
+                    Vivian@gmail.com
+                  </div>
+                  <div className="attribute-fixed-width request-record">
+                    Appointment
+                  </div>
+                  <div className="attribute-fixed-width date-record">
+                    Today, 10 May 2023
+                  </div>
+                  <div className="delete-view-btn">
+                    <div className="view-button">View</div>
+                    <div className="delete-button">Delete</div>
+                  </div>
+                </div>
+                <div className="list-container">
+                  <div className="s-n">4</div>
+                  <div className="attribute-fixed-width patient-profile-record">
+                    <div className="profile-img-view"></div>
+                    <div className="name-view-profile-container">
+                      <p>Micheal Adekunle</p>
+                      <p className="colored-view-profile">View profile</p>
+                    </div>
+                  </div>
+                  <div className="attribute-fixed-width email-record">
+                    Vivian@gmail.com
+                  </div>
+                  <div className="attribute-fixed-width request-record">
+                    Appointment
+                  </div>
+                  <div className="attribute-fixed-width date-record">
+                    Today, 10 May 2023
+                  </div>
+                  <div className="delete-view-btn">
+                    <div className="view-button">View</div>
+                    <div className="delete-button">Delete</div>
+                  </div>
+                </div>
+                <div className="list-container">
+                  <div className="s-n">5</div>
+                  <div className="attribute-fixed-width patient-profile-record">
+                    <div className="profile-img-view"></div>
+                    <div className="name-view-profile-container">
+                      <p>Micheal Adekunle</p>
+                      <p className="colored-view-profile">View profile</p>
+                    </div>
+                  </div>
+                  <div className="attribute-fixed-width email-record">
+                    Vivian@gmail.com
+                  </div>
+                  <div className="attribute-fixed-width request-record">
+                    Appointment
+                  </div>
+                  <div className="attribute-fixed-width date-record">
+                    Today, 10 May 2023
+                  </div>
+                  <div className="delete-view-btn">
+                    <div className="view-button">View</div>
+                    <div className="delete-button">Delete</div>
+                  </div>
+                </div>
+                <div className="list-container">
+                  <div className="s-n">6</div>
+                  <div className="attribute-fixed-width patient-profile-record">
+                    <div className="profile-img-view"></div>
+                    <div className="name-view-profile-container">
+                      <p>Micheal Adekunle</p>
+                      <p className="colored-view-profile">View profile</p>
+                    </div>
+                  </div>
+                  <div className="attribute-fixed-width email-record">
+                    Vivian@gmail.com
+                  </div>
+                  <div className="attribute-fixed-width request-record">
+                    Appointment
+                  </div>
+                  <div className="attribute-fixed-width date-record">
+                    Today, 10 May 2023
+                  </div>
+                  <div className="delete-view-btn">
+                    <div className="view-button">View</div>
+                    <div className="delete-button">Delete</div>
+                  </div>
+                </div>
+                <div className="list-container">
+                  <div className="s-n">7</div>
+                  <div className="attribute-fixed-width patient-profile-record">
+                    <div className="profile-img-view"></div>
+                    <div className="name-view-profile-container">
+                      <p>Micheal Adekunle</p>
+                      <p className="colored-view-profile">View profile</p>
+                    </div>
+                  </div>
+                  <div className="attribute-fixed-width email-record">
+                    Vivian@gmail.com
+                  </div>
+                  <div className="attribute-fixed-width request-record">
+                    Appointment
+                  </div>
+                  <div className="attribute-fixed-width date-record">
+                    Today, 10 May 2023
+                  </div>
+                  <div className="delete-view-btn">
+                    <div className="view-button">View</div>
+                    <div className="delete-button">Delete</div>
+                  </div>
+                </div>
+                <div className="list-container">
+                  <div className="s-n">8</div>
+                  <div className="attribute-fixed-width patient-profile-record">
+                    <div className="profile-img-view"></div>
+                    <div className="name-view-profile-container">
+                      <p>Micheal Adekunle</p>
+                      <p className="colored-view-profile">View profile</p>
+                    </div>
+                  </div>
+                  <div className="attribute-fixed-width email-record">
+                    Vivian@gmail.com
+                  </div>
+                  <div className="attribute-fixed-width request-record">
+                    Appointment
+                  </div>
+                  <div className="attribute-fixed-width date-record">
+                    Today, 10 May 2023
+                  </div>
+                  <div className="delete-view-btn">
+                    <div className="view-button">View</div>
+                    <div className="delete-button">Delete</div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>

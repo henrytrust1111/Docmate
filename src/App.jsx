63f72a95b4203de251077ef6c7e6e35cac8d1e40
henrytrust1111 <henrytrust1111@gmail.com>
2024-02-25 08:30:15 +0100

    changed the routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,49 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import './App.css'
-import PaymentMethod from './Components/BankTransfer/PaymentMethod'
-import Test from './Components/Test'
-import AdminDashboard from './Components/Dashboard/AdminDashboard'
-import PatientDashboard from './Components/PatientDashboard/PatientDashboard'
-
-
-const route=createBrowserRouter([
-  {
-    path:"/",
-    element:<AdminDashboard />
-  },
-  {
-    path:"/patientDashboard",
-    element:<PatientDashboard/>
-  },
-  {
-    path:"/payment",
-    element:<PaymentMethod/>
-  }
-])
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
+import "./App.css";
+import PaymentMethod from "./Components/BankTransfer/PaymentMethod";
+import Test from "./Components/Test";
+import AdminDashboard from "./Components/Dashboard/AdminDashboard";
+import PatientDashboard from "./Components/PatientDashboard/PatientDashboard";
 
 function App() {
   return (
-    <RouterProvider  router={route}/>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PatientDashboard />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/payment" element={<PaymentMethod />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
+
+// const route=createBrowserRouter([
+//   {
+//     path:"/",
+//     element:<AdminDashboard />
+//   },
+//   {
+//     path:"/patientDashboard",
+//     element:<PatientDashboard/>
+//   },
+//   {
+//     path:"/payment",
+//     element:<PaymentMethod/>
+//   }
+// ])
+
+// function App() {
+//   return (
+//     <RouterProvider  router={route}/>
+//   )
+// }
+
+// export default App
